ee9a5e9ce7499deb0b2b1f8196ce11f095039d3a
DeepAlgoExpert <beaumarfdeep0821@gmail.com>
2024-03-28 13:51:18 -0500

    http to https

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://100.26.246.56",
+  baseURL: "https://100.26.246.56",
   headers: {
     "Content-type": "application/json"
   }
