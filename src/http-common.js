f46a55e6e6062d03b39082691dc8664bb353dd0c
DeepAlgoExpert <beaumarfdeep0821@gmail.com>
2024-03-29 07:53:43 -0500

    online server url change

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://100.26.246.56",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://100.26.246.56",
+  //baseURL: "http://localhost:5000",
   headers: {
     "Content-type": "application/json"
   }
