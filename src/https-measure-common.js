c191f083aff5ed68e4e8eae11a0a3ba45a25fd12
DeepAlgoExpert <beaumarfdeep0821@gmail.com>
2024-05-06 18:30:32 -0500

    tab container max width setting

--- src/https-measure-common.js
+++ src/https-measure-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://3.89.143.205",
+  baseURL: "https://52.207.54.44",
   headers: {
     "Content-type": "application/json"
   }
