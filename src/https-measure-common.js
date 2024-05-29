fc9c2fff292c47fe8f9e0317e79e18bfbe4abc57
DeepAlgoExpert <beaumarfdeep0821@gmail.com>
2024-05-29 11:55:58 +0300

    common

--- src/https-measure-common.js
+++ src/https-measure-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://34.238.50.101",
+  baseURL: "https://54.175.70.202",
   headers: {
     "Content-type": "application/json"
   }
